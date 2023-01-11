e5507423a28776c0c161e9c74b1f5363e926ed9a
Harald Schult Ulriksen <harald.schult.ulriksen@aurum.no>
2023-01-11 08:06:21 +0100

    Init

---
+++ Brreg.fs
@@ -0,0 +1,39 @@
+﻿module Brreg
+
+open FSharp.Data
+
+[<Literal>]
+let searchSample = "https://data.brreg.no/enhetsregisteret/api/enheter?navn=Sesam&fraRegistreringsdatoEnhetsregisteret=2010-10-20&tilRegistreringsdatoEnhetsregisteret=2023-01-09&konkurs=false"
+let searchUrl = "https://data.brreg.no/enhetsregisteret/api/enheter?forretningsadresse.landkode=NO&fraRegistreringsdatoEnhetsregisteret=2014-10-20&tilRegistreringsdatoEnhetsregisteret=2023-01-09&konkurs=false&navn="
+
+type BregSearch = JsonProvider<searchSample>
+
+
+
+let search searchTerm = 
+    let result = BregSearch.Load(searchUrl + searchTerm)
+    match result.Page.TotalElements with
+    | 0 -> Array.empty
+    | _ ->  result.Embedded.Enheter 
+            |> Array.map (fun enhet -> {| OrgNr = enhet.Organisasjonsnummer; Navn = enhet.Navn |} )
+    
+
+open System.Text.Json
+open System.Text.Json.Serialization
+
+let options = JsonSerializerOptions()
+options.Converters.Add(JsonFSharpConverter())
+options.WriteIndented <- true
+
+
+[<Literal>]
+let enhetSample = "https://data.brreg.no/enhetsregisteret/api/enheter/914159350"
+let enhetsUrl = "https://data.brreg.no/enhetsregisteret/api/enheter/"
+type BregEnhet = JsonProvider<enhetSample>
+
+let companyDetails orgnr = 
+    let result = BregEnhet.Load(enhetsUrl + orgnr)
+    
+    let adresse = {| Adresse = result.Forretningsadresse.Adresse; Postnummer = result.Forretningsadresse.Postnummer; Sted = result.Forretningsadresse.Poststed; |}
+    JsonSerializer.Serialize ({| OrgNr = result.Organisasjonsnummer; Navn = result.Navn; Forretningsadresse = adresse; |}, options)
+    
---
+++ SpectreFSharp.fs
@@ -0,0 +1,157 @@
+﻿module Spectre.Console.FSharp
+
+open Spectre.Console 
+open Spectre.Console.Json
+open Spectre.Console.Rendering
+
+type Alignement = | Left | Center | Right
+
+type FigletConfig = 
+     {
+         Aligned: Alignement
+         Color: Spectre.Console.Color
+         Text: string
+     }
+
+type FigletBuilder() = 
+     member _.Yield _ = 
+         {
+             Text = ""
+             Color = Spectre.Console.Color.Red
+             Aligned = Left
+         }
+
+     member _.Run(config) = 
+        
+        let text = new FigletText(config.Text)
+        match config.Aligned with
+            | Left -> text.Justification <- Justify.Left
+            | Center -> text.Justification <- Justify.Center
+            | Right -> text.Justification <- Justify.Right
+        text.Color <- config.Color
+        text |> AnsiConsole.Write
+        ()
+
+     [<CustomOperation "text">]
+     member _.Text(state, text) = { state with Text = text }
+ 
+     [<CustomOperation "color">]
+     member _.Color(state, color) = { state with Color = color}
+
+     [<CustomOperation "aligned">]
+     member _.Aligned(state, alignement) = { state with Aligned = alignement}
+
+
+type Choice = Choice of string
+
+type SelectionConfig = 
+    {
+        Title: string
+        PageSize: int
+        MoreChoicesText: string
+        Choices: Choice list
+    }
+
+
+type SelectionBuilder() =
+    member _.Yield(_) = 
+        {
+            Title = ""
+            PageSize = 5
+            MoreChoicesText = "More"
+            Choices = list.Empty
+        }
+    member _.Run(config) = 
+        let prompt = SelectionPrompt<string>()
+        prompt.Title <- config.Title
+        prompt.PageSize <- config.PageSize
+        prompt.MoreChoicesText <- config.MoreChoicesText 
+        prompt.AddChoices(config.Choices |> Seq.map (fun (Choice choice) -> choice)) |> ignore
+        prompt |> AnsiConsole.Prompt |> Choice
+
+    [<CustomOperation("title")>]
+    member _.Title(state, title) = { state with Title = title }
+
+    [<CustomOperation("morceChoicesText")>]
+    member _.MorceChoices(state, morechoicestext) = { state with MoreChoicesText = morechoicestext }
+
+    [<CustomOperation("pageSize")>]
+    member _.PageSize(state, pageSize) = { state with PageSize = pageSize }
+
+    [<CustomOperation("choices")>]
+    member _.Choices(state, choices) = { state with Choices = choices }
+
+type TextPromptConfig = 
+    {
+        Question: string
+        Validation: (string -> ValidationResult) option
+    }
+type TextPromptBuilder() = 
+    member _.Yield(_) = 
+        {
+            Question = ""
+            Validation = None
+        }
+    member _.Run(config) =
+        let prompt = new TextPrompt<string>(config.Question)
+        match config.Validation with
+        | None -> ()
+        | Some validation -> 
+            prompt.Validator <- validation
+        prompt |> AnsiConsole.Prompt<string> 
+
+    [<CustomOperation("question")>]
+    member _.Question(state, question) = { state with Question = question }
+
+    [<CustomOperation("validation")>]
+    member _.Validation(state, validation) = { state with Validation = validation}
+type ErrorConfig = 
+    {
+        Description: string
+    }
+type ErrorBuilder() =
+    member _.Yield(_) = 
+        {
+            Description = ""
+        }
+    member _.Run(config) =
+        AnsiConsole.Write(new Markup($"[red]{config.Description}[/]") :> Renderable)
+        AnsiConsole.WriteLine()
+        ()
+    [<CustomOperation("description")>]
+    member _.Description(state, error) = { state with Description = error}
+
+type JsonViewConfig = 
+    {
+        Json: string
+        Header: string
+    }
+type JsonViewBuilder() = 
+    member _.Yield(_) = 
+        {
+            Json = ""
+            Header = ""
+        }
+    member _.Run(config: JsonViewConfig) =
+        let jsonText = new JsonText(config.Json)
+        let panel = new Panel(jsonText)
+        panel.Header <- new PanelHeader(config.Header)
+        panel.RoundedBorder().Collapse().BorderColor(Color.Yellow) |> AnsiConsole.Write
+        ()
+    
+    [<CustomOperation("json")>]
+    member _.Json(state, json) = { state with Json = json }
+    
+    [<CustomOperation("header")>]
+    member _.Header(state, header) = { state with Header = header }
+
+type AnsiConsole() = 
+    
+    member _.Return(x) = 
+        x
+let error = ErrorBuilder()
+let jsonview = JsonViewBuilder()
+let textPrompt = TextPromptBuilder()
+let selection = SelectionBuilder()
+let figlet = FigletBuilder()
+let console = AnsiConsole()
