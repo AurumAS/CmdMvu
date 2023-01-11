e4564c99452efb35625b83d0b42ff310ab888d0e
Harald Schult Ulriksen <harald.schult.ulriksen@aurum.no>
2023-01-11 19:49:52 +0100

    Formatting, typo

--- Brreg.fs
+++ Brreg.fs
@@ -3,37 +3,50 @@
 open FSharp.Data
 
 [<Literal>]
-let searchSample = "https://data.brreg.no/enhetsregisteret/api/enheter?navn=Sesam&fraRegistreringsdatoEnhetsregisteret=2010-10-20&tilRegistreringsdatoEnhetsregisteret=2023-01-09&konkurs=false"
-let searchUrl = "https://data.brreg.no/enhetsregisteret/api/enheter?forretningsadresse.landkode=NO&fraRegistreringsdatoEnhetsregisteret=2014-10-20&tilRegistreringsdatoEnhetsregisteret=2023-01-09&konkurs=false&navn="
+let private searchSample =
+    "https://data.brreg.no/enhetsregisteret/api/enheter?navn=Sesam&fraRegistreringsdatoEnhetsregisteret=2010-10-20&tilRegistreringsdatoEnhetsregisteret=2023-01-09&konkurs=false"
+let private searchUrl =
+    "https://data.brreg.no/enhetsregisteret/api/enheter?forretningsadresse.landkode=NO&fraRegistreringsdatoEnhetsregisteret=2014-10-20&tilRegistreringsdatoEnhetsregisteret=2023-01-09&konkurs=false&navn="
 
-type BregSearch = JsonProvider<searchSample>
+type private BregSearch = JsonProvider<searchSample>
 
-
-
-let search searchTerm = 
+let search searchTerm =
     let result = BregSearch.Load(searchUrl + searchTerm)
+
     match result.Page.TotalElements with
     | 0 -> Array.empty
-    | _ ->  result.Embedded.Enheter 
-            |> Array.map (fun enhet -> {| OrgNr = enhet.Organisasjonsnummer; Navn = enhet.Navn |} )
-    
+    | _ ->
+        result.Embedded.Enheter
+        |> Array.map (fun enhet ->
+            {| OrgNr = enhet.Organisasjonsnummer
+               Navn = enhet.Navn |})
 
 open System.Text.Json
 open System.Text.Json.Serialization
 
-let options = JsonSerializerOptions()
+let private options = JsonSerializerOptions()
 options.Converters.Add(JsonFSharpConverter())
 options.WriteIndented <- true
 
 
 [<Literal>]
-let enhetSample = "https://data.brreg.no/enhetsregisteret/api/enheter/914159350"
-let enhetsUrl = "https://data.brreg.no/enhetsregisteret/api/enheter/"
-type BregEnhet = JsonProvider<enhetSample>
+let private enhetSample =
+    "https://data.brreg.no/enhetsregisteret/api/enheter/914159350"
+let private enhetsUrl = "https://data.brreg.no/enhetsregisteret/api/enheter/"
 
-let companyDetails orgnr = 
+type private BregEnhet = JsonProvider<enhetSample>
+
+let companyDetails orgnr =
     let result = BregEnhet.Load(enhetsUrl + orgnr)
-    
-    let adresse = {| Adresse = result.Forretningsadresse.Adresse; Postnummer = result.Forretningsadresse.Postnummer; Sted = result.Forretningsadresse.Poststed; |}
-    JsonSerializer.Serialize ({| OrgNr = result.Organisasjonsnummer; Navn = result.Navn; Forretningsadresse = adresse; |}, options)
-    
+
+    let adresse =
+        {| Adresse = result.Forretningsadresse.Adresse
+           Postnummer = result.Forretningsadresse.Postnummer
+           Sted = result.Forretningsadresse.Poststed |}
+
+    JsonSerializer.Serialize(
+        {| OrgNr = result.Organisasjonsnummer
+           Navn = result.Navn
+           Forretningsadresse = adresse |},
+        options
+    )
